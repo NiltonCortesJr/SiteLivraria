60f80b4f1dad303a7a8a1429d6a61b12a430f558
Izadora Ferreira <izadorasantos20@hotmail.com>
2022-06-07 17:18:56 -0300

    inicializando a page Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { FormsEditar, FormsInserir, Home } from "./pages";
+import { /*FormsEditar, FormsInserir,*/ Home } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add" element={<FormsInserir />} />
-        <Route path="/editar/:id" element={<FormsEditar />} />
+         <Route path="/home" element={<Home />} /> 
+         <Route path="/add" element={<FormsInserir />} />
+         <Route path="/editar/:id" element={<FormsEditar />} />
       </Routes>
     </BrowserRouter>
   );
