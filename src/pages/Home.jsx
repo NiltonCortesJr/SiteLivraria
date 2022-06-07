3af041ca1fd7fc88e3d0c22a440bc805e607f7da
Izadora Ferreira <izadorasantos20@hotmail.com>
2022-06-07 20:21:44 -0300

    construção da página Home com adição de imagem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from '../components/Header/Header'
+import HomeImg from "../components/HomeImg/HomeImg"
 import Cards from '../components/Cards/Cards'
 import Footer from '../components/Footer/Footer'
 
@@ -7,8 +8,12 @@ function Home() {
   return (
     <div className="home">
       <Header />
+       <HomeImg />
+
+       <p className="populares">Conheça os livros mais populares da semana</p>
         <Cards />      
       <Footer/>
+
     </div>
   )
 }
