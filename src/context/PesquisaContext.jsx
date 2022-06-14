88d3207daced4e8b6095634e4c89dfb7d989e73b
José Etchichury <jcetchichury@hotmail.com>
2022-06-14 20:35:54 -0300

    retirada de arquivos inuteis

--- src/context/PesquisaContext.jsx
+++ src/context/PesquisaContext.jsx
@@ -4,9 +4,8 @@ import { createContext } from "react";
 export const PesquisaContext = createContext ()
 
 export function PesquisaProvider ({children}) {
-    const [pesquisa, setPesquisa] = useState ([])
     const [value, setValue] = useState ("")
     return (
-        <PesquisaContext.Provider value={{pesquisa, setPesquisa, value, setValue}}> {children} </PesquisaContext.Provider>
+        <PesquisaContext.Provider value={{value, setValue}}> {children} </PesquisaContext.Provider>
     )
 }
